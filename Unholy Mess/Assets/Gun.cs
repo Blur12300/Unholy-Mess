9eb9335c1b5267a48062f6a00474b7b4993940c3
Blur12300 <pottingerjake@gmail.com>
2023-02-15 11:54:28 +0000

    C35 MK3 added

--- Unholy Mess/Assets/Gun.cs
+++ Unholy Mess/Assets/Gun.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class Gun : MonoBehaviour
 {
+    bool CanShoot = true;
+    bool isAimed = false;
+
+    public Vector3 adsPos;
+    public Vector3 hipPos;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +19,19 @@ public class Gun : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (!isAimed && !CanShoot && Input.GetKey(KeyCode.Mouse1))
+        {
+            transform.position = adsPos;
+            isAimed = true;
+        }
+        if (CanShoot && Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Shoot();
+        }
+    }
+    void Shoot()
+    {
+        GetComponentInChildren<ParticleSystem>().Play();
         
     }
 }
